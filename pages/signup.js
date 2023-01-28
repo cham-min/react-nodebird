9297716d34a9dc8ee60dbba0e2d7688656c8c88c
최현민 <hymn9613@gmail.com>
2023-01-28 21:36:06 +0900

    :sparkles: Add menu bar

--- pages/signup.js
+++ pages/signup.js
@@ -1,8 +1,17 @@
 import React from "react";
+import Head from "next/head";
+
 import AppLayout from "../components/AppLayout";
 
 const Signup = () => {
-  return <AppLayout>Signup page</AppLayout>;
+  return (
+    <>
+      <Head>
+        <title>회원가입 | NodeBird</title>
+      </Head>
+      <AppLayout>회원가입</AppLayout>
+    </>
+  );
 };
 
 export default Signup;
