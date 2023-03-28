fa0d179d9305c3600d3e9e04f4597f38c7b26baa
최현민 <hymn9613@gmail.com>
2023-03-28 23:32:57 +0900

    :bug: Fix _document.js file error

--- front/pages/_document.js
+++ front/pages/_document.js
@@ -1,12 +1,11 @@
 import React from "react";
-import Document, { Head, Html, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
-
     try {
       ctx.renderPage = () =>
         originalRenderPage({
@@ -31,12 +30,14 @@ export default class MyDocument extends Document {
   }
 
   render() {
-    <Html>
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>;
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
